4e44749abc31e5b8d55f1d8588e9bbca42389e13
CHANWOO LEE <chw210601@gmail.com>
2023-11-08 09:29:29 +0900

    dp 1 문제 추가

--- cpp/p13701.cpp
+++ cpp/p13701.cpp
@@ -3,9 +3,12 @@
 
 using namespace std;
 
-bitset<33445533> bs;
+bitset<33554433> bs;
 
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int n;
     while(1){
         cin >> n;
---
+++ cpp/test123.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string s = "😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍";
+    cout << s << endl;
+}
