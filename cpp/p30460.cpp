1b7bbf0612b8e06390e0c77aa155ef408fd2de67
CHANWOO LEE <chw210601@gmail.com>
2023-11-13 09:56:47 +0900

    dp 1 문제 추가

--- cpp/p30460.cpp
+++ cpp/p30460.cpp
@@ -1,135 +1,31 @@
-//시도 중
+#include <iostream>
+#include <algorithm>
 
-// // // #include <iostream>
-// // // #include <algorithm>
-// // // #include <vector>
-// // // using namespace std;
+using namespace std;
 
-// // // int main(){
-// // //     int N;
-// // //     cin >> N;
-// // //     vector<int> arr;
-// // //     vector<int> dp;
-// // //     int tmp,tmp2,tmp3,tmp4;
-    
-// // //     for(int i=0;i<3;i++){
-// // //         arr.push_back(0);
-// // //         dp.push_back(0);
-// // //     }
-// // //     for(int i=0;i<N;i++){
-// // //         cin >> tmp;
-// // //         arr.push_back(tmp);
-// // //     }
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N;
+    cin >> N;
+    int arr[200000+3] = {0};
+    int dp[200000+3] = {0};
+    for(int i=1;i<N+1;i++){
+        cin >> arr[i];
+    }
 
-// // //     for (int i = 3; i < arr.size(); i++){
-// // //         tmp =   max(dp[i-3]+arr[i] +arr[i-1]+arr[i-2] ,dp[i-3] + 2 * ( arr[i] + arr[i-1] + arr[i-2]));
-// // //         tmp2 =  max(dp[i-3]+2 *(arr[i] +arr[i-1])+arr[i-2] ,dp[i-3] + 2 *  arr[i] + arr[i-1] + arr[i-2]);
-// // //         dp.push_back(max(tmp,tmp2));
-// // //         // cout << dp[i] << endl;
-// // //     }
-// // //     for (int i = 0;i<arr.size();i++){
-// // //         cout << dp[i] << " ";
-// // //     }
-// // //         cout << dp[arr.size() - 1];
-// // //     return 0;
-// // // }
+    for(int i=1;i<3;i++){
+        dp[i] = INT32_MIN;
+        dp[i] = max(dp[i], dp[i - 1] + arr[i]);   
+    }
+    for(int i=3;i<N+3;i++){
+        dp[i] = INT32_MIN;
+        dp[i] = max(dp[i], dp[i - 3] + 2 * (arr[i - 2] + arr[i - 1] + arr[i]));
+        dp[i] = max(dp[i] , dp[i-1] + arr[i]);
+    }
+    cout << dp[N+2] << endl;
 
+    return 0;
 
-// // #include <iostream>
-// // #include <bitset>
-// // #include <cstring>
-
-// // using namespace std;
-
-// // bitset<200004> bs;
-// // int arr[200004];
-// // int dp[200004];
-
-// // int main() {
-// //     ios_base::sync_with_stdio(false);
-// //     cin.tie(NULL);
-// //     cout.tie(NULL);
-// //     int N;
-// //     cin >> N;
-// //     long long ans = 0;
-
-// //     for (int i = 1; i <= N; i++) {
-// //         cin >> arr[i];
-// //         if (arr[i] >= 0) {
-// //             bs[i] = 1;
-// //         }
-// //     }
-// //     bs[N + 1] = 1;
-// //     bs[N + 2] = 1;
-
-// //     for (int i = 1; i <= N; i++) {
-// //         dp[i] = arr[i] + arr[i + 1] + arr[i + 2];
-// //     }
-
-// //     for (int i = 1; i <= N;) {
-// //         // cout << "i:"<<i << endl;
-// //         if (bs[i] == 1 && bs[i + 1] == 1 && bs[i + 2] == 1) {
-// //             ans += 2 * dp[i];
-// //             i += 3; 
-// //         } else if (dp[i] > 0 && dp[i] >= dp[i + 1] && dp[i] >= dp[i + 2]) {
-// //             ans += 2 * dp[i];
-// //             i += 3; 
-// //         } else if (dp[i] > 0 && dp[i] <= dp[i+1] && dp[i+1] <= dp[i+2]){
-// //             ans += 2 * dp[i];
-// //             i += 3;
-// //         }
-// //         else {
-// //             ans += arr[i];
-// //             i++;
-// //         }
-// //         // cout << ans << endl;
-// //     }
-
-// //     // for(int i=1;i<=N;i++){
-// //     //     cout << dp[i] << " ";
-// //     // }
-
-// //     cout << ans;
-// // }
-
-// #include <iostream>
-// #include <vector>
-// #include <algorithm>
-
-// using namespace std;
-
-// int main() {
-//     ios_base::sync_with_stdio(false);
-//     cin.tie(NULL);
-    
-//     int N;
-//     cin >> N;
-    
-//     vector<int> arr(N + 3);
-//     vector<int> sum(N + 3);
-//     vector<long long> dp(N + 3, 0);
-
-//     for (int i = 1; i <= N; i++) {
-//         cin >> arr[i];
-//     }
-
-//     long long ans = 0;
-//     for(int i=1;i<=N;i++){
-//         sum[i]=arr[i]+arr[i+1]+arr[i+2];
-//     }
-
-//     for (int i = N; i >= 1; i--) {
-//         su
-//         dp[i] = max(dp[i], 0LL); 
-
-//         ans += dp[i];
-
-//         if (dp[i] > 0) {
-//             i -= 2;
-//         }
-//     }
-
-//     cout << ans;
-
-//     return 0;
-// }
+}
--- cpp/tempCodeRunnerFile.cpp
+++ cpp/tempCodeRunnerFile.cpp
@@ -1,19 +1,3 @@
-int i = s1.length();
-    int j = s2.length();
-    string lcs = "";
-    while(i>0 && j>0){
-        if(s1[i-1] == s2[j-1]){
-            lcs = s1[i-1] + lcs;
-            i--;
-            j--;
-        }
-        else{
-            if(dp[i-1][j] > dp[i][j-1]){
-                i--;
-            }
-            else{
-                j--;
-            }
-        }
-    }
-    
+    // for(int i=0;i<N+3;i++){
+    //     cout << dp[i] << " ";
+    // }
