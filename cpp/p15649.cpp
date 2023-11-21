ece21ec27d8edd17d69abe4874335b94dc1fe9f5
CHANWOO LEE <chw210601@gmail.com>
2023-11-22 08:38:50 +0900

    백트래킹  1 문제 추가

--- cpp/p15649.cpp
+++ cpp/p15649.cpp
@@ -1,18 +1,21 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
-
-int backtracking(int N, int M, int arr[], int index){
+int backtracking(int N, int M, int arr[], int ans[], int index){
     if(index == M){
         for(int i=0;i<M;i++){
-            cout << arr[i] << " ";
+            cout << ans[i] << " ";
         }
         cout << "\n";
         return 0;
     }
     for(int i=0;i<N;i++){
-        arr[index] = i+1;
-        backtracking(N, M, arr, index+1);
+        if(arr[i] != -1){
+            ans[index] = arr[i];
+            arr[i] = -1;
+            backtracking(N, M, arr, ans, index+1);
+            arr[i] = ans[index];
+        }
     }
     return 0;
 }
@@ -20,13 +23,10 @@ int backtracking(int N, int M, int arr[], int index){
 int main(){
     int N, M;
     cin >> N >> M;
-
     int arr[N];
+    int ans[M];
     for(int i=0;i<N;i++){
         arr[i] = i+1;
     }
-    backtracking(N, M, arr, 0);
-
-
-
+    backtracking(N, M, arr, ans, 0);
 }
