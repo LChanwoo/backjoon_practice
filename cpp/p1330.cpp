744cffd8d7d9d6cda25fe1c114313b31e58cd750
CHANWOO LEE <chw210601@gmail.com>
2024-01-13 19:43:39 +0900

    구현 문제 추가

---
+++ cpp/p1330.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    if(a>b)
+        cout << ">";
+    else if(a<b)
+        cout << "<";
+    else
+        cout << "==";
+}
