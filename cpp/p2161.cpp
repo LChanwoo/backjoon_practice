c768ae115127431b6337c4ecb55005951cbf318e
CHANWOO LEE <chw210601@gmail.com>
2024-01-31 11:00:58 +0900

    큐 문제 추가

---
+++ cpp/p2161.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+#include<queue>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    queue<int> q;
+    for (int i = 1; i <= n;i++){
+        q.push(i);
+    }
+    while(q.size()!=1){
+        cout << q.front() << ' ';
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+    cout << q.front();
+    return 0;
+}
