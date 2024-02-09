d761e258bc8f41ee2498cc0f204df59f42933279
CHANWOO LEE <chw210601@gmail.com>
2024-02-09 20:22:14 +0900

    출력 문제 추가

---
+++ cpp/p5597.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a[30] = {0,};
+    for (int i = 0; i < 28;i++){
+        int n;
+        cin >> n;
+        a[n - 1] = 1;
+    }
+    for (int i = 0; i < 30;i++){
+        if(a[i]==0){
+            cout << i + 1 << '\n';
+        }
+    }
+    return 0;
+}
