f9a24a56f1ebeaf938ad745327d30db720c4561d
CHANWOO LEE <chw210601@gmail.com>
2024-02-17 10:07:24 +0900

    출력문제 추가

---
+++ cpp/p3003.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int k, q, l, b, kn, p;
+    cin >> k >> q >> l >> b >> kn >> p;
+    cout << 1-k << " " << 1-q << " " << 2-l << " " << 2-b << " " << 2-kn << " " << 8-p;
+}
