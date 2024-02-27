d7067a99b71031ccd351e8a5617860b1dc9149fa
CHANWOO LEE <chw210601@gmail.com>
2024-02-27 11:29:27 +0900

    출력문제 추가

---
+++ cpp/p2439.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    for (int i = 0; i < N;i++){
+        for (int j = 0; j < N - i - 1;j++){
+            cout << " ";
+        }
+        for (int j = 0; j < i + 1;j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
